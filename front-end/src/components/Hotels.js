36efec69e9c06dcd2c9774bea1f604b7d4bef717
Harikrishna <harikrishna.pariveda@sjsu.edu>
2017-11-20 01:25:19 -0800

    Added hotel

---
+++ front-end/src/components/Hotels.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Route, withRouter } from 'react-router-dom';
+class Hotels extends Component {
+
+    render() {
+        return (
+            <div className="row justify-content-md-center">
+            Hotels
+            </div>
+        );
+    }
+}
+
+export default withRouter(Hotels);
