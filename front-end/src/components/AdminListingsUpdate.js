5cb90ef448208d7fa35e2921cf53fedd437046c6
Harikrishna <harikrishna.pariveda@sjsu.edu>
2017-11-21 01:33:37 -0800

    admin update listing

---
+++ front-end/src/components/AdminListingsUpdate.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import HomePageSearchTabs from "./HomePageSearchTabs";
+class AdminListings extends Component {
+
+    render() {
+        return (
+            <div className="row justify-content-md-center">
+              <div className="row col-md-8 justify-content-md-center">
+                update
+              </div>
+            </div>
+        );
+    }
+}
+
+export default AdminListings;
