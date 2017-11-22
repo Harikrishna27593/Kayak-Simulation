b23a593f540a86bb518ab4cf8da8f457e8c15a8b
Harikrishna <harikrishna.pariveda@sjsu.edu>
2017-11-22 02:38:09 -0800

    Admin view of Billings

---
+++ front-end/src/components/AdminBills.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class AdminUsers extends Component {
+
+    render() {
+        return (
+            <div className="row justify-content-md-center">
+            Bills info
+            </div>
+        );
+    }
+}
+
+export default AdminUsers;
