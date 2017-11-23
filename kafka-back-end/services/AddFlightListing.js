9f723f09f2a9bd0c044020bf01b3e4ba861f2d47
Harikrishna <harikrishna.pariveda@sjsu.edu>
2017-11-23 03:43:16 -0800

    Admin add a new flight service

--- kafka-back-end/services/AddFlightListing.js
+++ kafka-back-end/services/AddFlightListing.js
@@ -1,30 +1,22 @@
-var mongo = require("./mongo");
-var mongoURL = "mongodb://localhost:27017/KAYAK";
+var MongoConPool=require("./MongoConPool");
 function handle_request(msg, callback){
     var res = {};
-    mongo.connect(mongoURL, function(){
-        console.log('Connected to mongo at: ' + mongoURL);
-        var coll = mongo.collection('FlightListings');
-
-        coll.findOne({FlightId: msg.FlightId, Operator:msg.Operator, DepartureTime:msg.depttime ,DepartureDate:msg.departdate,ArrivalTime: msg.arrivaltime, ArrivalDate:msg.arrivaldate, Origin:msg.placefrom,Destination:msg.placeto}, function(err,flight){
+        var queryJson={FlightId: msg.FlightId, Operator:msg.Operator, DepartureTime:msg.depttime ,DepartureDate:msg.departdate,ArrivalTime: msg.arrivaltime, ArrivalDate:msg.arrivaldate, Origin:msg.placefrom,Destination:msg.placeto};
+    MongoConPool.findOne('FlightListings',queryJson, function(err,flight){
             if (flight) {
                 console.log("Flight Listing Already Exists");
                 res.code = "500";
                 res.value = "Flight Listing Already Exists";
                 console.log("2: "+res.code);
                 callback(null, res);
-
             } else {
-
-                coll.insertOne({FlightId: msg.FlightId, Operator:msg.Operator, DepartureTime:msg.depttime ,DepartureDate:msg.departdate,ArrivalTime: msg.arrivaltime, ArrivalDate:msg.arrivaldate, Origin:msg.placefrom,Destination:msg.placeto},{w: 1}, function(err, flight){
+                MongoConPool.insert('FlightListings',queryJson,function(err, flight){
                     if (flight.insertedCount>0) {
 
                         console.log("New Flight Listing Added");
                         //res.redirect(201,'/login');
                         res.code = "201";
                         res.value = "Success Flight Listing";
-
-
                     } else {
                         //res.status(400);
                         console.log("New Flight Listing failed");
@@ -35,12 +27,7 @@ function handle_request(msg, callback){
                     callback(null, res);
                 });
             }
-
         });
-
-
-    });
-
 }
 
 exports.handle_request = handle_request;
