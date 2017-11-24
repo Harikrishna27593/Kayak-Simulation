cf777e46e40489f52d7def1543e6c0977bf5da13
Harikrishna <harikrishna.pariveda@sjsu.edu>
2017-11-24 12:27:18 -0800

    added hotels fetch logic

--- kafka-back-end/services/addhotellisting.js
+++ kafka-back-end/services/addhotellisting.js
@@ -6,35 +6,42 @@ function addhotel(msg, callback){
         console.log("In handle request:"+ JSON.stringify(msg));
         mongo.connect(mongoURL, function(){
         var coll = mongo.collection('HotelListings');
-        var hotelId=Math.floor(Math.random() * 900);
-        coll.find({Name:msg.Name,Address:msg.Address,City:msg.City,State:msg.State}).toArray(function (err,result) {
+        coll.find({Name:msg.HotelName,Address:msg.HotelAddress,City:msg.HotelCity,State:msg.HotelState}).toArray(function (err,result) {
             console.log('----------------------------');
             console.log(result.length);
             console.log('----------------------------');
         if(result.length===0)
         {
-            coll.insertOne({hotelId:hotelId,Name:msg.Name,Address:msg.Address,City:msg.City,State:msg.State,Rooms:{singleRoomPrice:msg.Rooms.singleRoomPrice,doubleRoomPrice:msg.Rooms.doubleRoomPrice,suitRoomPrice:msg.Rooms.suitRoomPrice},Availability:msg.Availability,hotelsDateFrom:msg.hotelsDateFrom,hotelsDateTo:msg.hotelsDateTo,Ratings:'',Reviews:''},function(err, hotel){
+            coll.insertOne({hotelId:msg.HotelId,Name:msg.HotelName,Address:msg.HotelAddress,City:msg.HotelCity,State:msg.HotelState,Availability:"Yes",Ratings:'',Reviews:''},function(err, hotel){
                 if (hotel.insertedCount>0) {
                     console.log("inserted into HotelListings");
                     //res.redirect(201,'/login');
                     res.code = "201";
                     res.value = "Success Inserted into hotelLisitng";
+
                 } else {
                     //res.status(400);
                     console.log("document not inserted");
                     res.code = "400";
                     res.value = "failure insertion";
                 }
+                    console.log(res);
+                    console.log("asdasd");
+                    callback(null, res);
             });
+
         }
             else
         {
             console.log("document not inserted");
             res.code = "401";
             res.value = "Duplicate record";
+
+            callback(null, res);
         }
+
         });
-        callback(null, res);
+
     });
 }
 
--- kafka-back-end/services/hotelDetails.js
+++ kafka-back-end/services/hotelDetails.js
@@ -1,24 +1,26 @@
 var mongo = require("./mongo");
 var mongoURL = "mongodb://localhost:27017/KAYAK";
+//var winston = require('winston');
 function hotel_details(msg, callback){
-
     var res = {};
     console.log("In handle request:"+ JSON.stringify(msg));
     mongo.connect(mongoURL, function(){
         console.log('Connected to mongo at: ' + mongoURL);
-        console.log("ASSSSSSSSSSS");
-        console.log(msg.hotelsDateFrom);
         var from=msg.hotelsDateFrom;
         var to=msg.hotelsDateTo;
         var coll = mongo.collection('HotelListings');
-        coll.find({City: msg.City, hotelsDateFrom: {$gte:from},hotelsDateTo: {$lt:to}}).toArray( function(err, hotel){
-res=hotel;
-//console.log(res.length);
-callback(null, res);
+        coll.find({City: msg.City, hotelsDateFrom: {$gte:from},hotelsDateTo: {$lt:to}}).toArray( function(err, hotel)
+        {
+            res=hotel;
+            callback(null, res);
         });
     });
 }
 function gethoteldetails(msg, callback){
+
+    // winston.remove(winston.transports.File);
+    // winston.add(winston.transports.File, { filename: './public/LogFiles/KayakAnalysis.json' });
+    // winston.log('info', 'Hotel Page Viewed', { page_name : 'Hotels_page'});
 
     var res = {};
     console.log("In handle request:"+ JSON.stringify(msg));
