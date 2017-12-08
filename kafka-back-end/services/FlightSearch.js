5ad73f8c485f057081e47548f79608f90a8378ae
Harikrishna <harikrishna.pariveda@sjsu.edu>
2017-12-07 23:39:10 -0800

    modified kafka-backend files

--- kafka-back-end/services/FlightSearch.js
+++ kafka-back-end/services/FlightSearch.js
@@ -2,8 +2,8 @@ var mongo = require("./mongo");
 var MongoConPool=require("./MongoConPool");
 var mongoURL = "mongodb://localhost:27017/KAYAK";
 var winston = require('winston');
- //winston.add(winston.transports.File, { filename: './public/LogFiles/KayakAnalysis.json' });
- //winston.remove(winston.transports.Console);
+//winston.add(winston.transports.File, { filename: './public/LogFiles/KayakAnalysis.json' });
+//winston.remove(winston.transports.Console);
 function handle_request(msg, callback){
 
     //winston.remove(winston.transports.File);
@@ -11,52 +11,66 @@ function handle_request(msg, callback){
     winston.add(winston.transports.File, { filename: './public/LogFiles/KayakAnalysis.json' });
     winston.log('info', 'Flight Page Viewed', { page_name : 'Flights_page'});
 
-
     var res = {};
     var i=0;
     try {
 
-            var queryJson={Origin: msg.placefrom,Destination:msg.placeto,DepartureDate:msg.departdate};
+        var queryJson={Origin: msg.placefrom,Destination:msg.placeto,DepartureDate:msg.departdate};
 
-            MongoConPool.find('FlightListings',queryJson,function(err, flights){
-                if (err) {
-                    res.code = "401";
-                    res.value = "Flights details fetch unsuccessful";
-                    callback(null, res);
-                }
-                else {
-                    var resArr = [];
-                    resArr = flights.map(function (file) {
-                        var flightsJSON = {};
-                        flightsJSON.id=flights[i].FlightId;
-                        flightsJSON.operator = flights[i].Operator;
-                        flightsJSON.origin=flights[i].Origin;
-                        flightsJSON.destination=flights[i].Destination;
-                        //flightsJSON.doorCount=flights[i].Doors;
-if(msg.flightCabin=="Business")
+        MongoConPool.find('FlightListings',queryJson,function(err, flights){
+            if (err) {
+                res.code = "401";
+                res.value = "Flights details fetch unsuccessful";
+                callback(null, res);
+            }
+            else {
+                var resArr = [];
+                resArr = flights.map(function (file) {
+                    var flightsJSON = {};
+                    flightsJSON.id=flights[i].FlightId;
+                    flightsJSON.operator = flights[i].Operator;
+                    flightsJSON.origin=flights[i].Origin;
+                    flightsJSON.destination=flights[i].Destination;
+                    //flightsJSON.doorCount=flights[i].Doors;
+                    if(msg.flightCabin=="Business")
                         flightsJSON.flightPrice=flights[i].Business;
-else if(msg.flightCabin=="First")
-    flightsJSON.flightPrice=flights[i].FirstClass;
-else
-    flightsJSON.flightPrice=flights[i].Economy;
-                        flightsJSON.DepartureDate=flights[i].DepartureDate;
-                        flightsJSON.DepartureTime=flights[i].DepartureTime;
-                        flightsJSON.ArrivalDate=flights[i].ArrivalDate;
-                        flightsJSON.ArrivalTime=flights[i].ArrivalTime;
-flightsJSON.type=flights[i].Type;
-flightsJSON.JourneyTime=flights[i].TotalTime;
-                        i=i+1;
-                        return flightsJSON;
+                    else if(msg.flightCabin=="First")
+                        flightsJSON.flightPrice=flights[i].FirstClass;
+                    else
+                        flightsJSON.flightPrice=flights[i].Economy;
+                    flightsJSON.DepartureDate=flights[i].DepartureDate;
+                    flightsJSON.DepartureTime=flights[i].DepartureTime;
+                    flightsJSON.ArrivalDate=flights[i].ArrivalDate;
+                    flightsJSON.ArrivalTime=flights[i].ArrivalTime;
+                    flightsJSON.type=flights[i].Type;
+                    flightsJSON.JourneyTime=flights[i].TotalTime;
+                    i=i+1;
+                    return flightsJSON;
+                });
+                res.code = "200";
+                res.value = "FlightsListing Successful";
+                res.arr=resArr;
+                console.log('---------------');
+                console.log(res);
+                console.log('---------------');
+                console.log(msg.user)
+                if (msg.user!=undefined) {
+                    MongoConPool.insert("UserTracking",{username: msg.user,Pagename:"Flight Listings",sessionid:msg.sid,Date:new Date()}, function (err, user) {
+                        if (user.insertedCount > 0) {
+
+
+                        } else {
+
+                        }
+
                     });
-                    res.code = "200";
-                    res.value = "FlightsListing Successful";
-                    res.arr=resArr;
-                    console.log('---------------');
-                    console.log(res);
-                    console.log('---------------');
-                    callback(null, res);
+
                 }
-            });
+
+
+                callback(null, res);
+            }
+        });
 
     }
     catch (e){
--- kafka-back-end/services/UpdateHotelListing.js
+++ kafka-back-end/services/UpdateHotelListing.js
@@ -6,11 +6,22 @@ function handle_request(msg, callback){
         console.log('Connected to mongo at: ' + mongoURL);
         var coll = mongo.collection('HotelListings');
         var res = {};
-        var updateJson={Name:msg.HotelName,Address:msg.HotelAddress,City:msg.HotelCity,State:msg.HotelState};
-        coll.update({hotelId: msg.InitialHotelId},{$set : updateJson},function(err, hotels) {
-            if (hotels) {
+        console.log('---------------IN UPDATE HOTEL LISTINGS');
+        console.log(msg);
+        console.log('---------------IN UPDATE HOTEL LISTINGS');
+        //
+        // updateSingleRoomPrice: '150',
+        //     after handle in server.js Success Hotel updation
+        // updateDoubleRoomPrice: 410,
+        //     updatesuitRoomPrice: 534,
 
+
+
+        var updateJson={Name:msg.HotelName,Address:msg.HotelAddress,hotelsDateFrom:msg.HotelsDateFrom.substr(0,10),hotelsDateTo:msg.HotelsDateTo.substr(0,10),City:msg.HotelCity,State:msg.HotelState,Rooms:{singleRoomPrice:msg.updateSingleRoomPrice,doubleRoomPrice:msg.updateDoubleRoomPrice,suitRoomPrice:msg.updatesuitRoomPrice}};
+        coll.update({hotelId: parseInt(msg.HotelId)},{$set : updateJson},function(err, hotels) {
+            if (hotels) {
                 console.log("Hotel Listing updated");
+
                 //res.redirect(201,'/login');
                 res.code = "201";
                 res.value = "Success Hotel updation";
